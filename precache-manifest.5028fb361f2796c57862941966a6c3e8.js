737754f40a73bffabebd665f51976dd7fab757ed
David <dony7th@gmail.com>
2019-02-03 23:21:54 -0500

    Updates

---
+++ precache-manifest.5028fb361f2796c57862941966a6c3e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f16c03b14913732a742",
+    "url": "/contactapp/static/css/main.e07c9b41.chunk.css"
+  },
+  {
+    "revision": "2f16c03b14913732a742",
+    "url": "/contactapp/static/js/main.2f16c03b.chunk.js"
+  },
+  {
+    "revision": "e2ce852f5557ec54db02",
+    "url": "/contactapp/static/js/1.e2ce852f.chunk.js"
+  },
+  {
+    "revision": "adb34f852c58a9da01ea",
+    "url": "/contactapp/static/js/runtime~main.adb34f85.js"
+  },
+  {
+    "revision": "9677332df6d74b88f19481a6cc88db67",
+    "url": "/contactapp/index.html"
+  }
+];
